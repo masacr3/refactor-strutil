e04b922b44b39e3c32a19064fda323c7c856d76e
Leonel R <leorhcp2030@gmail.com>
2018-06-25 18:32:19 -0300

    Create pruebas.c

---
+++ pruebas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "strutil.h"
+
+int main(){
+  char* cadena = "hola como estas";
+  char** s = split("hola como estas",' ');
+  
+  fprintf(stdout, "cadena original: %s\n", cadena);
+  printf("\n");
+  
+  for(int i=0; s[i]; i++){
+    fprintf(stdout, "%s", s[i]);
+  }
+  
+  free_strv(s);
+  return 0;
+}
